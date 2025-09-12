85088d755fc0db911ba576959e7808425686968e
Suhil Jugroop <suhil0311@gmail.com>
2025-09-12 21:12:30 +0000

    I'm getting a firebase not configured when I rrun cypress on my local de

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from "cypress";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:9002',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      // Pass Next.js environment variables to Cypress
+      config.env.NEXT_PUBLIC_FIREBASE_API_KEY = process.env.NEXT_PUBLIC_FIREBASE_API_KEY;
+      config.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN = process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN;
+      config.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
+      config.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET;
+      config.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID = process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID;
+      config.env.NEXT_PUBLIC_FIREBASE_APP_ID = process.env.NEXT_PUBLIC_FIREBASE_APP_ID;
+
+      return config;
     },
   },
 });
