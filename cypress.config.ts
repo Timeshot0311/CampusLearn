3d7859ff50131b03c5d36336c7edfeee6d29754e
Suhil Jugroop <suhil0311@gmail.com>
2025-09-12 20:55:26 +0000

    use those scripts but for npm

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:9002',
-    video: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
