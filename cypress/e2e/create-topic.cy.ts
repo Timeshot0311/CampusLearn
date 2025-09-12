11269647b79c64615772b6287315f2d3ea987725
Suhil Jugroop <suhil0311@gmail.com>
2025-09-12 21:30:09 +0000

    you forgot to tell it to choose a course or mark it as general, that's w

--- cypress/e2e/create-topic.cy.ts
+++ cypress/e2e/create-topic.cy.ts
@@ -23,14 +23,14 @@ describe('Create Topic', () => {
     const topicTitle = `Cypress Test Topic - ${new Date().getTime()}`;
     const topicDescription = 'This is a test description for a topic created via a Cypress test.';
 
-    // *** FIX: Wait for the dialog to be fully open and interactive before proceeding ***
-    cy.contains('button', 'Create Topic').should('be.visible');
+    // Wait for the dialog to be fully open before proceeding
+    cy.contains('h2', 'Create a New Help Topic').should('be.visible');
 
     cy.get('#title').type(topicTitle);
     cy.get('#description').type(topicDescription);
     
-    // Assuming there's a course to select. If not, this needs adjustment.
-    cy.get('button[role="combobox"]').first().click();
+    // Click the course dropdown and select the first option
+    cy.get('button[role="combobox"]').click();
     cy.get('[role="option"]').first().click();
 
     // 4. Submit the form
